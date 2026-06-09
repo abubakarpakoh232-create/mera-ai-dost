87a9b9bbcb2d6c0202d67eb36ec15e70414eb8b9
abubakarpakoh232-create <abubakarpakoh232@gmail.com>
2026-06-08 20:23:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,5 @@ if prompt := st.chat_input("Kya haal hai?"):
     with st.chat_message("assistant"):
         response = model.generate_content(prompt)
         st.markdown(response.text)
-    st.session_state.messages.append({"role": "
+st.session_state.messages.append({"role": "user", "content": prompt})
+st.session_state.messages.append({"role": "assistant", "content": response.text})
