eed61544a130ad5a4ed82f326fa5921fd4a17f2b
abubakarpakoh232-create <abubakarpakoh232@gmail.com>
2026-06-08 22:26:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ if prompt := st.chat_input("Kya poochna hai?"):
 st.session_state.messages.append({"role": "user", "content": prompt})   
 
 # Line 26: AI ka response session state (history) mein save karein
-st.session_state.messages.append({"role": "assistant", "content": response})
+st.session_state.messages.append({"role": "assistant", "content": mera-ai-dost})
+                                
 
 # Line 28: Thoda sa gap ya divider (Optional)
 st.divider()
